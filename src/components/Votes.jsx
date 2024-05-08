79e0a74f3df2870ecbfdc46f4db3dd389e913cb3
Daniel Williams <autoboots@gmail.com>
2024-05-08 10:16:35 +0100

    adds state to handle hasVoted and isVoting error - disables button if voted, displays error message if error

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -3,25 +3,47 @@ import { patchVotesByArticleId } from "../utils/api";
 
 const Votes = ({ id, votes }) => {
     const [votesData, setVotesData] = useState(votes);
+    const [hasVoted, setHasVoted] = useState(false);
+    const [isVotingError, setIsVotingError] = useState(false);
 
     const handleVote = (inc) => {
         setVotesData((prevVotesData) => prevVotesData + inc);
+        setHasVoted(true);
         patchVotesByArticleId(id, { inc_votes: inc })
-            .then(({ data: { article } }) => {
-                const updatedVotes = article.votes;
-                // setVotesData(updatedVotes);
+            .then(() => {
                 console.log("Votes updated");
             })
             .catch((err) => {
+                setIsVotingError(true);
+                setHasVoted(false);
+                setVotesData(votes);
                 console.log("error updating votes", err);
             });
     };
 
     return (
         <>
-            <button onClick={() => handleVote(1)}>🔼</button>
-            <p>{votesData} Votes</p>
-            <button onClick={() => handleVote(-1)}>🔽</button>
+            <section className="Votes">
+                <p
+                    className={
+                        isVotingError
+                            ? "Votes__error-true"
+                            : "Votes__error-false"
+                    }
+                >
+                    Voting error, please try again later
+                </p>
+
+                <button disabled={hasVoted} onClick={() => handleVote(1)}>
+                    🔼
+                </button>
+                <p>
+                    {votesData} {votesData === 1 ? "Vote" : "Votes"}
+                </p>
+                <button disabled={hasVoted} onClick={() => handleVote(-1)}>
+                    🔽
+                </button>
+            </section>
         </>
     );
 };
