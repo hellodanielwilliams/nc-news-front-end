ba579d2db6bcd835e572dffbf1264979d2d3e77a
Daniel Williams <autoboots@gmail.com>
2024-05-09 16:03:31 +0100

    adds error state to ArticleDisplay to display ErrorNotFound in case of non-existent article

--- src/components/ErrorNotFound.jsx
+++ src/components/ErrorNotFound.jsx
@@ -1,8 +1,14 @@
-const ErrorNotFound = () => {
+const ErrorNotFound = ({ type }) => {
     return (
         <section>
-            <h3>404 - Not found!</h3>
-            <p>Sorry, the requested URL was not found on this server.</p>
+            <h3>
+                404 - {type ? type[0].toUpperCase() + type.slice(1) : ""} Not
+                found!
+            </h3>
+            <p>
+                Sorry, the requested {type ? type : "URL"} was not found on this
+                server.
+            </p>
         </section>
     );
 };
